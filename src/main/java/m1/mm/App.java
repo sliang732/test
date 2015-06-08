7fbfb0614c91258de5eca5653ee1693e3978a7ec
sliang732 <sliang732@qq.com>
2015-06-08 17:27:10 +0800

    1

---
+++ src/main/java/m1/mm/App.java
@@ -0,0 +1,19 @@
+package m1.mm;
+
+import org.joda.time.LocalTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	LocalTime currentTime = new LocalTime();
+
+        System.out.println( "Hello World!" +currentTime);
+        
+        
+    }
+}
