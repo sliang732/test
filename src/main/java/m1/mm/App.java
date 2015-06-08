3886bc3be6302cb6e3557f6b995c4e5acbb429a1
sliang732 <sliang732@qq.com>
2015-06-08 17:32:13 +0800

    2

--- src/main/java/m1/mm/App.java
+++ src/main/java/m1/mm/App.java
@@ -12,7 +12,7 @@ public class App
     {
     	LocalTime currentTime = new LocalTime();
 
-        System.out.println( "Hello World!" +currentTime);
+        System.out.println( "Hello World!!!!!!!" +currentTime);
         
         
     }
